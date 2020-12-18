c50b56267e56a6f2d909be2f9cad1dc61294e617
ruqayya254 <72867417+ruqayya254@users.noreply.github.com>
2020-12-18 19:10:42 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,33 @@
+class Chain{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 1,
+        }
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+        
+            
+            stroke(48,22,8);
+
+            strokeWeight(5);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+           
+            
+            
+        }
+    }
+}
